497019295fa31e4042c93a8045ca4ed912bb0436
aaahana <e10991aaahana@dpsrkp.net>
2021-11-12 21:22:03 +0530

    initial

---
+++ test.py
@@ -0,0 +1,24 @@
+import turtle
+from random import randint
+
+turtle.speed(0)
+turtle.bgcolor('black')
+turtle.hideturtle()
+turtle.colormode(255)
+
+colors = []
+sides = randint(0, 11)
+print(sides)
+x = 1
+
+for _ in range(sides):
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    colors.append((255-r, 255-g, 255-b))
+
+while True:
+    turtle.pencolor(colors[x % sides])
+    turtle.fd(x)
+    turtle.lt(360/sides - 1)
+    x = x + 1
